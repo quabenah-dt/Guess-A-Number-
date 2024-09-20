c33c4e6e1687059aa2017e089d55358b20c67d89
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-20 19:31:42 +0000

    added hidden to the class of the board and the logo container

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const button = document.querySelector('.js-button');
+const board = document.querySelector('.board');
+const details = document.querySelector('.details');
+const guess = document.querySelector('.guess');
+const detailsButton = document.querySelector('.details-button');
+const guessButton = document.querySelector('.guess-button');
+const detailsInput = document.querySelector('.details-input');
+const guessInput = document.querySelector('.guess-input');
+const logoContainer = document.querySelector('.logo-container');
+
+
 
 if (button) {
     button.addEventListener('click', () => {
-        prompt('Button clicked');
+        board.classList.remove('hidden');
+        logoContainer.classList.add('hidden');
+        console.log('button clicked');
     });
 } else {
-    console.error('Button with class .js-button not found');
+    console.log('not hidden');
 }
 
-// document.querySelector('.js-button').addEventListener('click', () => {
-//     console.log('button clicked');
-// });

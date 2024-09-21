8d4a7a5c0dd4b086a8de72d745749f6ad8567a53
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-21 23:57:34 +0000

    did some changes to the css and the javascript

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-const button = document.querySelector('.js-button');
-const board = document.querySelector('.board');
-const details = document.querySelector('.details');
-const guess = document.querySelector('.guess');
-const detailsButton = document.querySelector('.details-button');
-const guessButton = document.querySelector('.guess-button');
-const detailsInput = document.querySelector('.details-input');
-const guessInput = document.querySelector('.guess-input');
-const logoContainer = document.querySelector('.logo-container');
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.js-button');
+    const board = document.querySelector('.board');
+    const details = document.querySelector('.details');
+    const guess = document.querySelector('.guess');
+    const detailsButton = document.querySelector('.details-button');
+    const guessButton = document.querySelector('.guess-button');
+    const detailsInput = document.querySelector('.details-input');
+    const guessInput = document.querySelector('.guess-input');
+    const logoContainer = document.querySelector('.logo-container');
 
-
-
-if (button) {
-    button.addEventListener('click', () => {
-        board.classList.remove('hidden');
-        logoContainer.classList.add('hidden');
-        console.log('button clicked');
-    });
-} else {
-    console.log('not hidden');
-}
-
+    if (button) {
+        button.addEventListener('click', () => {
+            board.classList.remove('hidden');
+            logoContainer.classList.add('hidden');
+            console.log('button clicked');
+        });
+    } else {
+        console.log('button not found');
+    }
+});

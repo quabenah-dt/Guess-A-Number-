fa9679cf78009f579b8d314018ecae50f1af4c38
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-22 00:38:19 +0000

    made some changes

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.js-button');
-    const board = document.querySelector('.board');
-    const logoContainer = document.querySelector('.logo-container');
 
-    if (button) {
-        button.addEventListener('click', () => {
-            board.classList.remove('hidden');
-            logoContainer.classList.add('hidden');
-            console.log('button clicked');
-        });
-    } else {
-        console.log('button not found');
-    }
+
+const button = document.querySelector('.js-button');
+const board = document.querySelector('.board');
+const logoContainer = document.querySelector('.logo-container');
+
+button.addEventListener('click', () => {
+    logoContainer.classList.add('hidden'); // Hide the logo container
+    board.classList.remove('hidden'); // Show the board
+    console.log('Play Game button clicked');
 });

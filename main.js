ff44430eaeb7325985047bbcac6fa5eb553c9f16
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-22 00:01:38 +0000

    deleted some things on javascript

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('.js-button');
     const board = document.querySelector('.board');
-    const details = document.querySelector('.details');
-    const guess = document.querySelector('.guess');
-    const detailsButton = document.querySelector('.details-button');
-    const guessButton = document.querySelector('.guess-button');
-    const detailsInput = document.querySelector('.details-input');
-    const guessInput = document.querySelector('.guess-input');
     const logoContainer = document.querySelector('.logo-container');
 
     if (button) {
